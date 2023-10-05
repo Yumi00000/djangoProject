7a1bde548f11b3bf6c4a36a978fb5968f039f922
Yumi00000 <vb34550@gmail.com>
2023-10-05 10:22:35 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,6 @@ from .models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
