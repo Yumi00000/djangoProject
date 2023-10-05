40610a5583e1e269e9cdb957f2088ca81d2f3573
Yumi00000 <vb34550@gmail.com>
2023-10-05 10:19:12 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- WebDjangoProject/settings.py
+++ WebDjangoProject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'WebDjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'blog/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
